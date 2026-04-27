af52f8ea8cefbe156cd06fc106b58e15636ea88c
simonator1001 <simonator1001@gmail.com>
2026-04-28 02:05:22 +0800

    feat: switch AppWrite to Singapore region (project 69efa226000db23fcd89)
    - Updated endpoint to sgp.cloud.appwrite.io/v1
    - Updated project ID and database ID
    - Created business_cards collection with all attributes

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, ID } from 'appwrite'
 
-const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://fra.cloud.appwrite.io/v1'
-const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '69ef2ce800308cf97330'
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://sgp.cloud.appwrite.io/v1'
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '69efa226000db23fcd89'
 
 const client = new Client()
   .setEndpoint(endpoint)
@@ -11,7 +11,7 @@ export const account = new Account(client)
 export const databases = new Databases(client)
 export { ID }
 
-export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || '69ef339f0038efc60a25'
+export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || 'bizcard_ai'
 export const CARDS_COLLECTION = 'business_cards'
 
 export default client
